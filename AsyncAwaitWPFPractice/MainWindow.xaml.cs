faaa2d6ea31940cd6ef2231e70d5600d3f9e0dcb
3472592 <3472592@gmail.com>
2024-06-27 07:29:28 -0400

    Add files via upload

---
+++ AsyncAwaitWPFPractice/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Threading;
+using System.Windows.Threading;
+using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitWPFPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer timer = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(1);
+        }
+
+        private int i = 0;
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            progress.Text = (i++).ToString();
+        }
+
+        private async void CountButton_Click(object sender, RoutedEventArgs e)
+        {
+            countButton.IsEnabled = false;
+            timer.Start();
+
+            if (useAwaitAsync.IsChecked == true)
+                await LongTaskAsync();
+            else
+                LongTask();
+            countButton.IsEnabled = true;
+        }
+
+        private void LongTask()
+        {
+            Thread.Sleep(5000);
+            timer.Stop();
+        }
+
+        private async Task LongTaskAsync()
+        {
+            await Task.Delay(5000);
+            timer.Stop();
+        }
+    }
+}
